3f972ba1f601670715126460802bc87ce1c6df69
Nguyen Huu Tuan <tuan.nguyen1@pn.com.vn>
2019-09-17 18:21:14 +0700

    finished

---
+++ app/src/main/java/com/example/androidlinearlist/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.androidlinearlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String[] equipments = new String[]{"Fridge", "Heater", "Fan", "Lamp", "Dryer", "AC"};
+
+        for (int i = 0; i < equipments.length; i++) {
+            Button newButton = new Button(MainActivity.this);
+            newButton.setText("+");
+            TextView newEquipment = new TextView(MainActivity.this);
+            newEquipment.setText(equipments[i]);
+            LinearLayout main_child = new LinearLayout(MainActivity.this);
+            main_child.addView(newEquipment);
+            main_child.addView(newButton);
+            LinearLayout main = findViewById(R.id.main_linear);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    1.0f
+            );
+            newEquipment.setLayoutParams(params);
+            main.addView(main_child);
+        }
+    }
+}
