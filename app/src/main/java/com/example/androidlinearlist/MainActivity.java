4b8afc00740a6a6e8c000563eef3ad030a54a2d9
Nguyen Huu Tuan <tuan.nguyen1@pn.com.vn>
2019-09-17 18:23:25 +0700

    small changes

--- app/src/main/java/com/example/androidlinearlist/MainActivity.java
+++ app/src/main/java/com/example/androidlinearlist/MainActivity.java
@@ -14,16 +14,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String[] equipments = new String[]{"Fridge", "Heater", "Fan", "Lamp", "Dryer", "AC"};
+        LinearLayout main = findViewById(R.id.main_linear);
 
         for (int i = 0; i < equipments.length; i++) {
             Button newButton = new Button(MainActivity.this);
             newButton.setText("+");
+
             TextView newEquipment = new TextView(MainActivity.this);
             newEquipment.setText(equipments[i]);
+
             LinearLayout main_child = new LinearLayout(MainActivity.this);
             main_child.addView(newEquipment);
             main_child.addView(newButton);
-            LinearLayout main = findViewById(R.id.main_linear);
+
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.MATCH_PARENT,
                     LinearLayout.LayoutParams.MATCH_PARENT,
